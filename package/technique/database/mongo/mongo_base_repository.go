5b62a669a1f9e168bd57a627a92a32b6e5c2100c
phongthien <giangnt2@gviet.com>
2023-09-03 14:51:52 +0700

    fix(mongo/base): base repository

--- package/technique/database/mongo/mongo_base_repository.go
+++ package/technique/database/mongo/mongo_base_repository.go
@@ -3,6 +3,7 @@ package mongo
 import (
 	"context"
 
+	"github.com/sigmaott/gest/package/technique/database/mongo"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
