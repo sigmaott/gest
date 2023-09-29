fd0b03d874bd8f39bd79b355f5fca0265a55fc42
phongthien <giangnt2@gviet.com>
2023-09-29 10:41:14 +0700

    fix(kafka): kafka config

--- package/extension/kafkafx/kafka.go
+++ package/extension/kafkafx/kafka.go
@@ -3,5 +3,13 @@ package kafkafx
 import "go.uber.org/fx"
 
 func Module() fx.Option {
-	return fx.Module("kafkafx", fx.Provide(RegisterKafkaHooks))
+	return fx.Module("kafkafx",
+		fx.Provide(
+			fx.Annotate(
+				func() *KafkaSubscriber {
+					return &KafkaSubscriber{}
+				},
+				fx.ResultTags(`name:"platformKafka"`))),
+
+		fx.Provide(RegisterKafkaHooks))
 }
