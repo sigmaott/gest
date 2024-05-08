07edfa1e5f3a73d5310eb3198628c2174532e9c5
phongthien <giangnt2@gviet.com>
2024-05-08 09:51:50 +0700

    fix(echofx): add custom middleware

--- package/extension/mongofx/mongo.go
+++ package/extension/mongofx/mongo.go
@@ -2,26 +2,28 @@ package mongofx
 
 import (
 	"context"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"go.uber.org/fx"
 )
 
-func ForRoot(ctx context.Context, uri string, databaseName string) fx.Option {
+// TODO builder
+
+func ForRoot(ctx context.Context, databaseName string, opts ...*options.ClientOptions) fx.Option {
 	return fx.Module("mongofx", fx.Provide(func() *mongo.Client {
-		return basicConnection(ctx, uri)
+		return basicConnection(ctx, opts...)
 	}, func(client *mongo.Client) *mongo.Database {
 		return ConnectToDatabase(ctx, client, databaseName)
 	}))
 }
 func ConnectToDatabase(ctx context.Context, client *mongo.Client, databaseName string) (db *mongo.Database) {
-
 	return client.Database(databaseName)
 }
 
-func basicConnection(ctx context.Context, uri string) (db *mongo.Client) {
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+func basicConnection(ctx context.Context, opts ...*options.ClientOptions) (db *mongo.Client) {
+	client, err := mongo.Connect(ctx, opts...)
 	if err != nil {
 		panic(err)
 	}
